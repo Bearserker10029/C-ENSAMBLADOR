03b227d846d4f30bc1a5027b52cd989608eb65b4
Bearserker10029 <kentjianbin@gmail.com>
2025-07-04 20:45:28 -0500

    Agregar archivo suma_vector.c para sumar dos vectores utilizando memoria dinámica

---
+++ MEMORIA DINÁMICA/suma_vector.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int i;
+    srand(time(NULL));
+    int tam = 16;
+    int vectorA[tam];
+
+    for (i = 0; i < tam; i++)
+    {
+        vectorA[i] = rand() % 100;
+    }
+
+    int *vectorB = (int *)malloc(tam * sizeof(int));
+
+    if (vectorB == NULL)
+    {
+        printf("No se inicializo la memoria para vectorB\n");
+        exit(1);
+    }
+    else
+    {
+        printf("Se inicializo la memoria para vectorB\n");
+    }
+
+    for (i = 0; i < tam; i++)
+    {
+        vectorB[i] = rand() % 100;
+    }
+
+    int *vectorsuma = (int *)malloc(tam * sizeof(int));
+
+    if (vectorsuma == NULL)
+    {
+        printf("No se inicializo la memoria para vectorsuma\n");
+        exit(1);
+    }
+    else
+    {
+        printf("Se inicializo la memoria para vectorsuma\n");
+    }
+
+    for (i = 0; i < tam; i++)
+    {
+        vectorsuma[i] = vectorA[i] + vectorB[i];
+    }
+    printf("Vector A: ");
+    for (i = 0; i < tam; i++)
+    {
+        printf("%d ", vectorA[i]);
+    }
+    printf("\nVector B: ");
+    for (i = 0; i < tam; i++)
+    {
+        printf("%d ", vectorB[i]);
+    }
+    printf("\nVector Suma: ");
+    for (i = 0; i < tam; i++)
+    {
+        printf("%d ", vectorsuma[i]);
+    }
+    printf("\n");
+    free(vectorB);
+    free(vectorsuma);
+    return 0;
+}
